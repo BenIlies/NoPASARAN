b5e5900237aa16d7739889dd0255101ec730eb43
Ilies Benhabbour <iliesbenh@gmail.com>
2022-11-08 11:36:48 +0300

    add parenthesis commands

--- modules/interpreters/action_interpreter.py
+++ modules/interpreters/action_interpreter.py
@@ -35,18 +35,18 @@ class ActionInterpreter(cmd.Cmd):
         raise Exception('Parsing error: argument "' + line + '" is unknown.')
 
     def do_call(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         nested_xstate_json = json.load(open('.'.join((parsed[0], 'json'))))
         nested_machine = machine.get_child_machine(nested_xstate_json)
         machine.trigger(nested_machine.start())
 
     def do_listen(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         machine.start_sniffer()
         machine.set_stack(parsed[0])
 
     def do_send(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         send(machine.get_variable(parsed[0]))
         serializable_packet = codecs.encode(pickle.dumps(machine.get_variable(parsed[0])), "base64").decode()
         if machine.root_machine.controller_protocol:
@@ -55,11 +55,11 @@ class ActionInterpreter(cmd.Cmd):
         machine.trigger('PACKET_SENT')
 
     def do_done(self, line, machine):
-        parsed = InterpreterParser.parse(line, 0)
+        parsed = InterpreterParser.old_parse(line, 0)
         machine.trigger('DONE')
 
     def do_handle_packets(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         timeout = False
         start_time = time.time()
         while (True):
@@ -89,14 +89,14 @@ class ActionInterpreter(cmd.Cmd):
             machine.trigger('TIMEOUT')
             
     def do_packet_filter(self, line, machine):
-        parsed = InterpreterParser.parse(line)
+        parsed = InterpreterParser.old_parse(line)
         for word in parsed:
             if word in machine.get_variables():
                 word = machine.get_variable(word)
         machine.set_sniffer_filter(parsed.join(' '))
         
     def do_packet_filter(self, line, machine):
-        parsed = InterpreterParser.parse(line)
+        parsed = InterpreterParser.old_parse(line)
         for counter in range (0, len(parsed)):
             if parsed[counter] in machine.get_variables():
                 parsed[counter] = machine.get_variable(parsed[counter])
@@ -104,91 +104,91 @@ class ActionInterpreter(cmd.Cmd):
 
 
     def do_pop(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         machine.discard_stack_packet(machine.get_variable(parsed[0]))
 
     def do_create_TCP_packet(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         machine.set_variable(parsed[0], create_TCP_packet())
 
     def do_set_IP_dst(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_IP_dst(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_IP_src(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_IP_src(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
         
     def do_set_TCP_sport(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_sport(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_TCP_dport(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_dport(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_TCP_seq(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_seq(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_TCP_flags(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_flags(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_TCP_ack(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_ack(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_set_TCP_payload(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_payload(machine.get_variable(parsed[0]), machine.get_variable(parsed[1]))
 
     def do_remove_TCP_payload(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         remove_TCP_payload(machine.get_variable(parsed[0]))
 
     def do_set_TCP_automatic_packet_seq(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         set_TCP_automatic_packet_seq(machine.get_variable(parsed[0]))
 
     def do_set_TCP_automatic_packet_ack(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         set_TCP_automatic_packet_ack(machine.get_variable(parsed[0]), machine.get_variable(parsed[1])[0])
 
     def do_get_packet_IP(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         machine.set_variable(parsed[0], get_IP_src(machine.get_variable(parsed[1])[0]))
 
     def do_get_packet_port(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         machine.set_variable(parsed[0], get_TCP_sport(machine.get_variable(parsed[1])[0]))
 
     def do_print_TCP_payload(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         print(machine.get_variable(parsed[0])[0]['TCP'].payload)
 
     def do_set_random_int(self, line, machine):
-        parsed = InterpreterParser.parse(line, 3)
+        parsed = InterpreterParser.old_parse(line, 3)
         machine.set_variable(parsed[0], set_random_int(parsed[1], parsed[2]))
     
     def do_set_random_float(self, line, machine):
-        parsed = InterpreterParser.parse(line, 3)
+        parsed = InterpreterParser.old_parse(line, 3)
         machine.set_variable(parsed[0], set_random_float(parsed[1], parsed[2]))
 
     def do_set_variable(self, line, machine):
-        parsed = InterpreterParser.parse(line, 2)
+        parsed = InterpreterParser.old_parse(line, 2)
         machine.set_variable(parsed[0], parsed[1])
 
     def do_set_finishing_event(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         machine.finishing_event = parsed[0]
 
     def do_return(self, line, machine):
-        InterpreterParser.parse(line, 0)
+        InterpreterParser.old_parse(line, 0)
         machine.return_to_previous_state()
 
     def do_wait_for_ready_control_link(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         timeout = False
         start_time = time.time()
         while (True):
@@ -205,7 +205,7 @@ class ActionInterpreter(cmd.Cmd):
             
             
     def do_wait_for_disconnecting_control_link(self, line, machine):
-        parsed = InterpreterParser.parse(line, 1)
+        parsed = InterpreterParser.old_parse(line, 1)
         timeout = False
         machine.root_machine.controller_protocol.disconnecting()
         start_time = time.time()
@@ -220,4 +220,3 @@ class ActionInterpreter(cmd.Cmd):
             machine.trigger('TIMEOUT')
         else:
             machine.trigger('CONTROL_LINK_DISCONNECTING')
-
--- modules/parsers/interpreter_parser.py
+++ modules/parsers/interpreter_parser.py
@@ -1,6 +1,6 @@
 class InterpreterParser():
     @staticmethod
-    def parse(parameters, correct_number=None):
+    def old_parse(parameters, correct_number=None):
         parsed_parameters = parameters.split()
         if correct_number == None:
             return parsed_parameters
@@ -8,3 +8,64 @@ class InterpreterParser():
             return parsed_parameters
         else:
             raise Exception('Parsing error: number of argument is incorrect expected ' + str(correct_number) + ' received ' + str(len(parsed_parameters)) + '.')
+
+    @staticmethod
+    def parse(command, input_args=None, output_args=None):
+        def checker(string):
+            depth = 0
+            for c in string:
+                if  c == '(':
+                    depth = depth + 1
+                elif  c == ')':
+                    depth = depth - 1
+                elif c != ' ' and depth == 0:
+                    raise Exception('Error value without parenthesis in ' + string + '.')
+                if depth < 0:
+                    raise Exception('Parenthesis closed without having one open in ' + string + '.')
+                elif depth > 1:
+                    raise Exception('Parenthesis can not be nested in ' + string + '.')
+            if depth > 0:
+                raise Exception('Number of closing and opening parenthesis are not matching in ' + string + '.')
+
+
+        def parenthetic_contents(string):
+            checker(string)
+            stack = []
+            for i, c in enumerate(string):
+                if c == '(':
+                    stack.append(i)
+                elif c == ')' and stack:
+                    start = stack.pop()
+                    yield string[start + 1: i]
+
+        
+        set_of_arguments = 0
+        if input_args != 0:
+            set_of_arguments = set_of_arguments + 1
+        if output_args != 0:
+            set_of_arguments = set_of_arguments + 1
+        args = list(parenthetic_contents(command))
+        if len(args) > set_of_arguments:
+            raise Exception('Mismatch in the input and output values in ' + command + ', got too many sets of arguments.')
+        if len(args) < set_of_arguments:
+            raise Exception('Mismatch in the input and output values in ' + command + ', got not enough sets of arguments.')
+
+        if set_of_arguments == 2:
+            inputs = args[0].split(' ')
+            outputs = args[1].split(' ')
+        elif set_of_arguments == 1:
+            if input_args == 0:
+                inputs = []
+                outputs = args[0].split(' ')
+            else:
+                inputs = args[0].split(' ')
+                outputs = []
+        else:
+            inputs = []
+            outputs = []
+        if input_args != None and input_args != len(inputs):
+            raise Exception('Number of inputs mismatch.')
+        if output_args != None and output_args != len(outputs):  
+            raise Exception('Number of outputs mismatch.')            
+        print(inputs, "AND", outputs)
+        return inputs, outputs

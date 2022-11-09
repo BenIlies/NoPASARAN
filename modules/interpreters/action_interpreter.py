849d5e478b1f1b952fb65bad77cc25696639ba1b
Ilies Benhabbour <iliesbenh@gmail.com>
2022-11-09 13:38:14 +0300

    get parameters tentative

--- modules/interpreters/action_interpreter.py
+++ modules/interpreters/action_interpreter.py
@@ -246,5 +246,5 @@ class ActionInterpreter(cmd.Cmd):
 
     def do_get_parameters(self, line, machine):
         _, outputs = InterpreterParser.parse(line, 0, 0, False, True)
-        for parameter in machine.parameters:
-            machine.set(outputs[0], parameter)
+        for index, parameter in machine.parameters:
+            machine.set_variable(outputs[index], parameter)

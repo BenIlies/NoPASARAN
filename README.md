c6506043577753e695a32bf85ffa655203394b8d
Ilies Benhabbour <iliesbenh@gmail.com>
2023-06-20 09:39:00 +0300

    README.md and main comments

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # NoPASARAN
+
+NoPASARAN is an advanced network tool designed to detect, fingerprint, and locate network middleboxes in a unified framework. Written in Python, NoPASARAN uses finite state machines to describe test cases and leverages Ansible for distributing and orchestrating these tests across a network of nodes.
+
+## Features
+
+* Detection, fingerprinting, and location of network middleboxes.
+* Utilization of finite state machines for test case description.
+* Network orchestration with Ansible.
+* Flexible roles for network nodes and proxies.
+* Support for JSON-based scenario files for state machine configurations.
+
+## Requirements
+
+* Python 3.8 or later
+* Ansible
+* Scapy
+* Twisted
+
+## Installation
+
+1. Clone the NoPASARAN repository:
+   ```bash
+   git clone https://github.com/BenIlies/NoPASARAN.git
+   ```
+
+2. Navigate into the NoPASARAN directory:
+   ```bash
+   cd NoPASARAN
+   ```
+
+3. Install the necessary Python packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+NoPASARAN can be run in either the Node or Proxy role using the main.py script:
+
+1. To run as a Node:
+   ```bash
+   python main.py NODE --scenario=<path-to-json-scenario-file>
+   ```
+
+2. To run as a Proxy:
+   ```bash
+   python main.py PROXY
+   ```
+
+Replace `<path-to-json-scenario-file>` with the path to your actual JSON scenario file.
--- main.py
+++ main.py
@@ -1,57 +1,39 @@
+# Import libraries
 import argparse
 import json
-
 from scapy.all import *
 from twisted.internet.threads import deferToThread
 from twisted.internet import reactor
-
 from modules.machines.machine import Machine
 from modules.ipsec_tunnels.ipsec_conf import NodeIpsecConf, ProxyIpsecConf
-
-#iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP 
-#iptables -L "chain" --line-numbers
-
-'''
-  NEW FEATURES TO ADD:
-  - RST and kernel
-  - Logger
-  - Exceptions
-
-
-gérer les erreurs / exception handler avec evenement --> ex: interface down donc pas de packet sent 
-
-
-DOCUMENTATION PAR FONCTIONS doctest
-https://docs.python.org/3/library/doctest.html
-
-https://docs.python-guide.org/writing/documentation/#:~:text=Sphinx,manual%20pages%2C%20and%20plain%20text
-
-PARTIE SUR LE SWITCH
--> ADAPTER LES CONNEXION TCP/ UTILISATION ADRESSE IP PRIVEE POUR LE TUNNEL IPSEC ?
-'''
   
 if __name__ == '__main__':
+  # Set up argument parser
   parser = argparse.ArgumentParser(
     prog='ROLE', 
-    epilog="See '<command> --help' to read about a specific sub-command."
+    epilog="See '<command> --help' for more information on a command."
   )
 
+  # Set up base parser
   base_parser = argparse.ArgumentParser(add_help=False)
-  subparsers = parser.add_subparsers(dest='role', help='role for testing the end to end connection')
 
-  node_parser = subparsers.add_parser("NODE", help="set the role of the node as an endpoint for testing the end to end connection", parents=[base_parser])
-  node_parser.add_argument("-s", "--scenario", required=True, help="JSON scenario file for the finite state machine")
+  # Create subparsers for the role 
+  subparsers = parser.add_subparsers(dest='role', help='Define role for testing the connection')
 
-  proxy_parser = subparsers.add_parser("PROXY", help="set the role of the node as a proxy for control link", parents=[base_parser])
-  controller_configuration = None
+  # Parser for NODE role
+  node_parser = subparsers.add_parser("NODE", help="Define node role for testing", parents=[base_parser])
+  node_parser.add_argument("-s", "--scenario", required=True, help="JSON scenario file for state machine")
 
+  # Parser for PROXY role
+  proxy_parser = subparsers.add_parser("PROXY", help="Define node role as a proxy", parents=[base_parser])
+
+  # Parse command line arguments
   args = parser.parse_args()
 
+  # If role is NODE, load JSON file and start the machine
   if args.role == 'NODE':
     xstate_json = json.load(open(args.scenario))
     machine = Machine(xstate_json=xstate_json)
     main_thread = deferToThread(machine.start)
     main_thread.addCallback(lambda _: reactor.stop())
     reactor.run()
-
-  

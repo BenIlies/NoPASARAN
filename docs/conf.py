458cd2091f899478e87ce2bc6585a5917182503a
Ilies Benhabbour <iliesbenh@gmail.com>
2023-07-06 11:04:19 +0300

    test

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+# Get the current directory
+current_directory = os.getcwd()
+
+# List the contents of the current directory
+directory_contents = os.listdir(current_directory)
+
+# Print the directory contents
+for item in directory_contents:
+    print(item)
+
 extensions = [
    'sphinx.ext.duration',
    'sphinx.ext.doctest',

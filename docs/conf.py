adeb0e5160eb7ec03185173ed73b042874e97f8f
Ilies Benhabbour <iliesbenh@gmail.com>
2023-07-08 09:27:03 +0300

    changement of theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,6 @@
 import os
 import sys
-import sphinx_pdj_theme
 sys.path.insert(0, os.path.abspath('../'))
-
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 extensions = [
    'sphinx.ext.duration',
@@ -16,3 +12,5 @@ extensions = [
 autosummary_generate = True
 
 templates_path = ['templates']
+
+html_theme = "sphinxawesome_theme"

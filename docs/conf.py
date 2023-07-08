984a6ab85171e0ca80e9258edc1223a72a30fe1a
Ilies Benhabbour <iliesbenh@gmail.com>
2023-07-08 09:12:01 +0300

    test template documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import sphinx_pdj_theme
 sys.path.insert(0, os.path.abspath('../'))
+
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 extensions = [
    'sphinx.ext.duration',

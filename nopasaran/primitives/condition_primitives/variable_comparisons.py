b510f738f8d2128c096b568a1019402cad269999
Ilies Benhabbour <iliesbenh@gmail.com>
2023-07-27 14:17:50 +0300

    add empty comparison

--- nopasaran/primitives/condition_primitives/variable_comparisons.py
+++ nopasaran/primitives/condition_primitives/variable_comparisons.py
@@ -134,3 +134,30 @@ class VariableComparisons:
             bool: True if the first value is less or equal, False otherwise.
         """
         return state_variables[inputs[0]] <= state_variables[inputs[1]]
+
+    @staticmethod
+    @parsing_decorator(input_args=1, output_args=0)
+    def empty(inputs, outputs, state_variables):
+        """
+        Check if the variable from the machine, whose name is provided as input, is None.
+
+        Number of input arguments: 1
+        
+        Number of output arguments: 0
+        
+        Optional input arguments: No
+        
+        Optional output arguments: No
+
+        Args:
+            inputs (List[str]): The list of input variable names. It contains one mandatory input argument, which is the name of the variable to be checked.
+            
+            outputs (List[str]): The list of output variable names.
+            
+            state_variables (dict): The dictionary of state variables.
+
+        Returns:
+            bool: True if the only variable from the machine is None, False otherwise.
+        """
+        variable_name = inputs[0]
+        return state_variables[variable_name] is None

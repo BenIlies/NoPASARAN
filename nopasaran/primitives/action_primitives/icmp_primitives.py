06aa2807186767aa1950151bd7c73706b2361810
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-03-20 13:53:34 +0300

    Update icmp_primitives.py

--- nopasaran/primitives/action_primitives/icmp_primitives.py
+++ nopasaran/primitives/action_primitives/icmp_primitives.py
@@ -7,6 +7,33 @@ class ICMPPrimitives:
     """
 
     @staticmethod
+    @parsing_decorator(input_args=0, output_args=1)
+    def create_ICMP_packet(inputs, outputs, state_machine):
+        """
+        Create an ICMP packet and store it in an output variable in the machine's state.
+
+        Number of input arguments: 0
+
+        Number of output arguments: 1
+
+        Optional input arguments: No
+
+        Optional output arguments: No
+
+        Args:
+            inputs (List[str]): The list of input variable names. This method does not use input variables.
+            
+            outputs (List[str]): The list of output variable names. It contains one mandatory output argument, which is
+                the name of the variable to store the created ICMP packet.
+            
+            state_machine: The state machine object.
+
+        Returns:
+            None
+        """
+        state_machine.set_variable_value(outputs[0], utils.create_ICMP_packet())
+
+    @staticmethod
     @parsing_decorator(input_args=1, output_args=1)
     def get_ICMP_payload(inputs, outputs, state_machine):
         """

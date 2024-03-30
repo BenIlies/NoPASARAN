a3df077339f74d25e5185251477747d95eca4e58
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-03-30 03:21:28 +0300

    Add print functions

--- nopasaran/primitives/action_primitives/data_manipulation.py
+++ nopasaran/primitives/action_primitives/data_manipulation.py
@@ -246,7 +246,7 @@ class DataManipulationPrimitives:
         Optional output arguments: No
 
         Args:
-            inputs (List[str]): The list of input variable names. It contains two mandatory input arguments:
+            inputs (List[str]): The list of input variable names. It contains one mandatory input argument:
                 - The name of a variable to be printed.
             
             outputs (List[str]): The list of output variable names.
@@ -257,4 +257,30 @@ class DataManipulationPrimitives:
             None
         """
         print(state_machine.get_variable_value(inputs[0]))
-        
+
+    @staticmethod
+    @parsing_decorator(input_args=1, output_args=0)
+    def print_packet(inputs, outputs, state_machine):
+        """
+        Print a packet.
+
+        Number of input arguments: 1
+
+        Number of output arguments: 0
+
+        Optional input arguments: No
+
+        Optional output arguments: No
+
+        Args:
+            inputs (List[str]): The list of input variable names. It contains one mandatory input argument:
+                - The name of a variable whose value is the packet to be printed.
+            
+            outputs (List[str]): The list of output variable names.
+            
+            state_machine: The state machine object.
+
+        Returns:
+            None
+        """
+        utils.print_packet(state_machine.get_variable_value(inputs[0]))

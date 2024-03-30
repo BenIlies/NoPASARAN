7e6571034be8a29924ded7da9a252b82919bd377
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-03-30 03:41:46 +0300

    Update io_primitives.py

--- nopasaran/primitives/action_primitives/io_primitives.py
+++ nopasaran/primitives/action_primitives/io_primitives.py
@@ -33,3 +33,31 @@ class IOPrimitives:
         """
         file_variables = json.load(open('.'.join((inputs[0], 'json'))))
         state_machine.set_variable_value(outputs[0], file_variables[inputs[1]])
+
+    @staticmethod
+    @parsing_decorator(input_args=2, output_args=0)
+    def print_to_file(inputs, outputs, state_machine):
+        """
+        Load variables from a file and store them in the machine's state.
+
+        Number of input arguments: 2
+
+        Number of output arguments: 0
+
+        Optional input arguments: No
+
+        Optional output arguments: No
+
+        Args:
+            inputs (List[str]): The list of input variable names. It contains two mandatory input arguments, which are the file path and the name of the variable whose contents are to be appended to the file.
+            
+            outputs (List[str]): The list of output variable names.
+            
+            state_machine: The state machine object.
+
+        Returns:
+            None
+        """
+
+        with open(inputs[0], 'a') as file:
+            file.write(inputs[0] + "\n")

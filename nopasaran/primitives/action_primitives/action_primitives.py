237a7a116502b823c56b55a56177d88729c3d405
Mahmoud Attia <attimoovy@gmail.com>
2024-12-17 11:45:26 +0300

    update exports

--- nopasaran/primitives/action_primitives/action_primitives.py
+++ nopasaran/primitives/action_primitives/action_primitives.py
@@ -16,8 +16,8 @@ from nopasaran.primitives.action_primitives.certificate_primitives import Certif
 from nopasaran.primitives.action_primitives.tls_primitives import TLSPrimitives
 from nopasaran.primitives.action_primitives.http_1_request_primitives import HTTP1RequestPrimitives
 from nopasaran.primitives.action_primitives.http_1_response_primitives import HTTP1ResponsePrimitives
-from nopasaran.primitives.action_primitives.http_2_request_primitives import HTTP2RequestPrimitives
-from nopasaran.primitives.action_primitives.http_2_response_primitives import HTTP2ResponsePrimitives
+from nopasaran.primitives.action_primitives.http_2_server_primitives import HTTP2ServerPrimitives
+from nopasaran.primitives.action_primitives.http_2_client_primitives import HTTP2ClientPrimitives
 
 class ActionPrimitives(Primitives):
     """
@@ -41,6 +41,6 @@ class ActionPrimitives(Primitives):
         TLSPrimitives,
         HTTP1RequestPrimitives,
         HTTP1ResponsePrimitives,
-        HTTP2RequestPrimitives,
-        HTTP2ResponsePrimitives
+        HTTP2ServerPrimitives,
+        HTTP2ClientPrimitives
         ]

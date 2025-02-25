28eb27ae7ed36a91d5902a931813ab8995764bfa
Mahmoud Attia <attimoovy@gmail.com>
2025-02-26 01:04:46 +0300

    .

--- nopasaran/http_2_utils.py
+++ nopasaran/http_2_utils.py
@@ -332,7 +332,7 @@ def send_headers_frame(conn: h2.connection.H2Connection, sock, frame_data: Dict,
         - id: Test case ID
     """
     next_legal_id = conn.get_next_available_stream_id()
-    stream_id = frame_data.get('stream_id', next_legal_id)
+    stream_id = frame_data.get('stream_id', 3)
     headers = frame_data.get('headers')
     duplicate_headers = frame_data.get('duplicate_headers')
 

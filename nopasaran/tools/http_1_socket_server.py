e30c724fb1ab204f6cd4458bc231ec27c46c9d92
benilies <iliesbenh@gmail.com>
2025-05-21 10:11:21 +0300

    modify http socket server to avoid dangling

--- nopasaran/tools/http_1_socket_server.py
+++ nopasaran/tools/http_1_socket_server.py
@@ -17,21 +17,45 @@ class HTTP1SocketServer:
         self.client_socket = None
         self.TIMEOUT = 5.0
 
-    def handle_client_connection(self, client_socket):
+    def handle_client_connection(self, client_socket, timeout):
         """
         Handle a client connection by processing the incoming request and sending the appropriate response.
         """
-        request = client_socket.recv(4096)
-        request_str = request.decode('utf-8')
+        client_socket.setblocking(False)
+        ready_to_read, _, _ = select.select([client_socket], [], [], timeout)
+        
+        if not ready_to_read:
+            client_socket.close()
+            return None, EventNames.TIMEOUT.name
+
+        try:
+            request = client_socket.recv(4096)
+        except socket.timeout:
+            client_socket.close()
+            return None, EventNames.TIMEOUT.name
+        except ConnectionResetError as e:
+            client_socket.close()
+            return str(e), EventNames.ERROR.name
+
+        if not request:
+            client_socket.close()
+            return None, EventNames.TIMEOUT.name
+
+        request_str = request.decode('utf-8', errors='ignore')
+
         # Extract request line and headers
         headers_end_index = request_str.find("\r\n\r\n")
         headers_part = request_str[:headers_end_index] if headers_end_index != -1 else request_str
         request_line = headers_part.split("\r\n")[0]
-        method, path, _ = request_line.split(" ", 2)
+
+        try:
+            method, path, _ = request_line.split(" ", 2)
+        except ValueError:
+            method, path = "GET", "/"
 
         route_key = (path, method)
         route_info_list = self.routes.get(route_key)
-        
+
         if route_info_list:
             response = ""
             for route_info in route_info_list:
@@ -39,41 +63,29 @@ class HTTP1SocketServer:
                 status_code = route_info.get('status', 200)
                 headers = route_info.get('headers', [])
 
-                # Construct each part of the response
                 response_part = f"HTTP/1.1 {status_code} OK\r\n"
                 for header_name, header_value in headers:
                     response_part += f"{header_name}: {header_value}\r\n"
-                response_part += f"\r\n{response_body}\r\n\r\n"  # Double CRLF to separate responses
+                response_part += f"\r\n{response_body}\r\n\r\n"
 
-                # Append the response part to the full response
                 response += response_part
 
-            # Send the full combined response to the client
             client_socket.sendall(response.encode())
-
         else:
             response_body = 'NoPASARAN HTTP/1.1 Server'
             status_code = 404
-            headers = []
-
-            # Construct the HTTP response
-            response = f"HTTP/1.1 {status_code} OK\r\n"
-            for header_name, header_value in headers:
-                response += f"{header_name}: {header_value}\r\n"
-            response += f"\r\n{response_body}"
-
-            # Send response to client
+            response = f"HTTP/1.1 {status_code} Not Found\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
             client_socket.sendall(response.encode())
 
         client_socket.close()
 
-        # Store the raw received request data
         self.received_request_data = request
 
-        # Notify that a request has been received
         if self.request_received:
             with self.request_received:
                 self.request_received.notify_all()
+
+        return request, EventNames.REQUEST_RECEIVED.name
 
     def wait_for_request(self, port, timeout):
         """
@@ -84,7 +96,7 @@ class HTTP1SocketServer:
             timeout (int): The timeout duration in seconds.
 
         Returns:
-            Tuple[bytes, str]: The raw received request data or None if a timeout occurs, and the event name.
+            Tuple[bytes or None, str]: The raw received request data or None if a timeout occurs, and the event name.
         """
         server_address = ('', port)
         self.request_received = threading.Condition()
@@ -96,22 +108,18 @@ class HTTP1SocketServer:
             server_socket.listen(1)
             server_socket.setblocking(False)
 
-            # Initialize the timeout timer
             start_time = time.time()
 
             while True:
-                # Check if the timeout has elapsed
                 elapsed_time = time.time() - start_time
-                if elapsed_time > timeout:
+                if elapsed_time >= timeout:
                     return None, EventNames.TIMEOUT.name
-                
-                # Use select to wait for a connection with a timeout
+
                 ready_to_read, _, _ = select.select([server_socket], [], [], timeout - elapsed_time)
-                
+
                 if ready_to_read:
                     client_socket, _ = server_socket.accept()
-                    self.handle_client_connection(client_socket)
-                    return self.received_request_data, EventNames.REQUEST_RECEIVED.name
+                    return self.handle_client_connection(client_socket, timeout - elapsed_time)
 
     def start(self, host, port):
         """
@@ -121,7 +129,7 @@ class HTTP1SocketServer:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((host, port))
         self.sock.listen(5)
-        
+
         return EventNames.SERVER_STARTED.name, f"Server successfully started at {host}:{port}."
 
     def close(self):
@@ -131,11 +139,10 @@ class HTTP1SocketServer:
                 self.client_socket.close()
             if self.sock:
                 self.sock.close()
-            
-            # Clear references
+
             self.client_socket = None
             self.sock = None
-            
+
             return EventNames.CONNECTION_ENDING.name
         except Exception as e:
             return EventNames.CONNECTION_ENDING.name
